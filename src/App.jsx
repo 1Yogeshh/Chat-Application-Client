033986cc20e3bd1d57db6c43f0b46f000a6555eb
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-01-31 20:47:57 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import Chat from "./pages/Chat"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import ChatPage from "./pages/Chat"
 import CreateUser from "./pages/CreateUser"
 import Login from "./pages/Login"
-import Profile from "./pages/Profile"
+import ProfilePage from "./pages/Profile"
 import Register from "./pages/Register"
 
 function App() {
     return (
         <>
-            {/* <Chat /> */}
-            <Profile isOwn />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/chat" />} />
+
+                    {/* AUTH */}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/create-user" element={<CreateUser />} />
+
+                    {/* MAIN APP */}
+                    <Route path="/chat" element={<ChatPage />} />
+
+                    {/* PROFILE */}
+                    <Route path="/profile" element={<ProfilePage isOwn />} />
+                    <Route path="/profile/:username" element={<ProfilePage />} />
+
+                    {/* FALLBACK */}
+                    <Route path="*" element={<Navigate to="/chat" />} />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
