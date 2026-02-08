7fd322ea6d279f83b5949622e5a45194a8d1cdc6
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-08 19:03:49 +0530

    add user slice file

---
+++ src/store/slices/user.Slice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserAPI, getMyProfileAPI } from "../../api/user.api"
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        profile: null,
+        loading: false,
+        error: null,
+        profileLoaded: false,
+    },
+    reducers: {
+        clearUser: (state) => {
+            state.profile = null,
+                state.profileLoaded = false
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            // CREATE USER
+            .addCase(createUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(createUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.profile = action.payload;
+                state.profileLoaded = true;
+            })
+            .addCase(createUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            // FETCH PROFILE
+            .addCase(fetchMyProfile.fulfilled, (state, action) => {
+                state.profile = action.payload;
+                state.profileLoaded = true;
+            });
+    }
+})
+
+export const { clearUser } = userSlice.actions;
+export default userSlice.reducer;
