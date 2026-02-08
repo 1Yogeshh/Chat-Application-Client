ac42550a6f3aea20386dc0d4978ac08ee5a76920
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-08 19:31:24 +0530

    changes in guestguard

--- src/guards/GuestGuard.jsx
+++ src/guards/GuestGuard.jsx
@@ -1,9 +1,21 @@
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const GuestGuard = ({ children }) => {
     const token = localStorage.getItem("accessToken");
+    const { profile, profileLoaded } = useSelector((s) => s.user)
+
+    // ⏳ profile abhi load ho rahi hai
+    if (token && !profileLoaded) {
+        return null; // ya loader
+    }
 
     if (token) {
+        return <Navigate to="/create-user" replace />;
+    }
+
+    // ✅ logged in + profile missing → create-user
+    if (token && !profile) {
         return <Navigate to="/create-user" replace />;
     }
 
