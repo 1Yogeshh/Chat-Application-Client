bcb6d5832ae8acc4fe89083e15cf02ffd589ba46
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-11 21:53:03 +0530

    add

--- src/components/chat/ChatLayout.jsx
+++ src/components/chat/ChatLayout.jsx
@@ -3,27 +3,27 @@ import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { useSelector } from "react-redux";
 
-const ChatLayout = ({ onUserClick }) => {
-  const { activeChatId, chats } = useSelector((s) => s.chat)
+const ChatLayout = () => {
+    const { activeChatId, chats } = useSelector((s) => s.chat)
 
-  // ❌ No chat selected
-  if (!activeChatId) {
+    // ❌ No chat selected
+    if (!activeChatId) {
+        return (
+            <div className="flex flex-1 items-center justify-center text-gray-400" >
+                Select a chat to start messaging
+            </div>
+        );
+    }
+
+    const currentChat = chats.find(c => c.id === activeChatId);
+
     return (
-      <div className="flex flex-1 items-center justify-center text-gray-400">
-        Select a chat to start messaging
-      </div>
+        <div className="flex flex-1 flex-col bg-[#F4F3F8] rounded-[30px]" >
+            <ChatHeader user={currentChat?.otherUser} />
+            <Messages chatId={activeChatId} />
+            <MessageInput chatId={activeChatId} />
+        </div>
     );
-  }
-
-  const currentChat = chats.find(c => c.id === activeChatId);
-
-  return (
-    <div className="flex flex-1 flex-col bg-[#F4F3F8] rounded-[30px]">
-      <ChatHeader user={currentChat?.otherUser} />
-      <Messages chatId={activeChatId} />
-      <MessageInput chatId={activeChatId} />
-    </div>
-  );
 };
 
 export default ChatLayout;
