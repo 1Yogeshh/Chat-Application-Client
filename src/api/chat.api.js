c63808bfca361e65c0fe77875f348c8f0c20bcc8
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-18 22:32:12 +0530

    changes in api

--- src/api/chat.api.js
+++ src/api/chat.api.js
@@ -1,18 +1,22 @@
 import api from "./axios";
 
+//get all chats
 export const getMyChatsAPI = () => {
     return api.get("/api/chats");
 }
 
+//get messages only
 export const getMessagesAPI = (chatId) => {
     console.log(chatId)
     return api.get(`/api/chat/${chatId}`);
 }
 
+//send message api
 export const sendMessageAPI = (data) => {
     return api.post("/api/chat/send", data);
 }
 
+//start and get private chat
 export const startPrivateChatAPI = (otherUserId) => {
     return api.get(`/api/chat/private/${otherUserId}`)
 }
