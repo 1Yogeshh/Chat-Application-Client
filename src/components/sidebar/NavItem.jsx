78fb57f17cb731569be0f2d88d4cdbc7dff5b6a1
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-22 20:30:29 +0530

    changes in navitem

--- src/components/sidebar/NavItem.jsx
+++ src/components/sidebar/NavItem.jsx
@@ -1,27 +1,3 @@
-// const NavItem = ({ icon, label, active = false, badge }) => {
-//   return (
-//     <div
-//       className={`flex items-center justify-between cursor-pointer transition ${
-//         active ? "text-white" : "hover:text-white"
-//       }`}
-//     >
-//       <div className="flex items-center gap-4">
-//         {icon}
-//         <span className="text-sm font-medium">{label}</span>
-//       </div>
-
-//       {badge && (
-//         <span className="bg-white text-[#1A0B2E] text-[10px] font-bold px-2 py-0.5 rounded-full">
-//           {badge}
-//         </span>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default NavItem;
-
-
 const NavItem = ({ icon, label, active = false, badge }) => {
     return (
         <div className={`
