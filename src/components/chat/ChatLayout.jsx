b6d36196796116f60f0c643f80421d89f87baaed
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2026-02-22 20:31:48 +0530

    Changes in chat layout

--- src/components/chat/ChatLayout.jsx
+++ src/components/chat/ChatLayout.jsx
@@ -1,39 +1,3 @@
-// import ChatHeader from "./ChatHeader";
-// import Messages from "./Messages";
-// import MessageInput from "./MessageInput";
-// import { useSelector } from "react-redux";
-
-// const ChatLayout = ({ onUserClick }) => {
-//     const { activeChatId, chats, onlineUsers } = useSelector((s) => s.chat)
-
-//     if (!activeChatId) {
-//         return (
-//             <div className="flex flex-1 items-center justify-center text-gray-400" >
-//                 Select a chat to start messaging
-//             </div>
-//         );
-//     }
-
-//     const currentChat = chats.find(c => c.id === activeChatId);
-
-//     const isOnline = onlineUsers[currentChat?.otherUser?.authUserId];
-
-//     return (
-//         <div className="flex flex-1 flex-col bg-[#F4F3F8] rounded-[30px]" >
-//             <ChatHeader
-//                 onUserClick={onUserClick}
-//                 user={currentChat?.otherUser}
-//                 isOnline={isOnline}
-//             />
-//             <Messages chatId={activeChatId} otherUser={currentChat?.otherUser} />
-//             <MessageInput chatId={activeChatId} />
-//         </div>
-//     );
-// };
-
-// export default ChatLayout;
-
-
 import ChatHeader from "./ChatHeader";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
